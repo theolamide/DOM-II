8bd185c0040de88557c197b0572a405160e155b2
Daniel Ajadi <ajadidan@gmail.com>
2019-10-01 18:30:39 -0400

    Mouse enter Mouse leave and Mouse over implemented

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
 // Your code goes here
+
+let headerAction = document.querySelector(".main-navigation");
+    headerAction.addEventListener("mouseenter",()=>{
+        headerAction.style.backgroundColor = "yellow";
+        headerAction.style.transition = "all 2s";
+    })
+    headerAction.addEventListener("mouseleave",()=>{
+        headerAction.style.backgroundColor = "white";
+        headerAction.style.transition = "all 1s";
+    })
+
+let navLink = document.getElementsByTagName("a");
+console.log(navLink);   
+    
+Array.from(navLink).forEach(item =>{
+    item.addEventListener("mouseover",()=>{
+        item.style.transform = "scale(1.5)"
+        item.style.transition = "1s"
+
+        setTimeout(function(){
+            item.style.transform  = "";
+        }, 1500)
+    })
+
+})
+
+
+
+// item.addEventListener("mouseenter", ()=>{
+//     item.style.transform = "scale(2)";
+//     item.style.transition = "all 0.5s";
+//   })
