45c22f9b2ee2140448f5251c5d20ae06fdd9f607
Daniel Ajadi <ajadidan@gmail.com>
2019-10-01 22:23:51 -0400

    Drag and Drop Implemented

--- js/index.js
+++ js/index.js
@@ -22,12 +22,67 @@ Array.from(navLink).forEach(item =>{
             item.style.transform  = "";
         }, 1500)
     })
-
 })
 
+//DRAG AND DROP
 
+const input = document.querySelector('input');
+const errorAlert = document.getElementById('errorAlert');
 
-// item.addEventListener("mouseenter", ()=>{
-//     item.style.transform = "scale(2)";
-//     item.style.transition = "all 0.5s";
-//   })
+input.addEventListener('keydown', logKey);
+
+function logKey(e){
+    errorAlert.textContent = "The search feature is not active yet"
+}
+
+var dragged;
+
+/* events fired on the draggable target */
+document.addEventListener("drag", function(event) {
+
+}, false);
+
+document.addEventListener("dragstart", function(event) {
+  // store a ref. on the dragged elem
+  dragged = event.target;
+  // make it half transparent
+  event.target.style.opacity = .5;
+}, false);
+
+document.addEventListener("dragend", function(event) {
+  // reset the transparency
+  event.target.style.opacity = "";
+}, false);
+
+/* events fired on the drop targets */
+document.addEventListener("dragover", function(event) {
+  // prevent default to allow drop
+  event.preventDefault();
+}, false);
+
+document.addEventListener("dragenter", function(event) {
+  // highlight potential drop target when the draggable element enters it
+  if (event.target.className == "dropzone") {
+    event.target.style.background = "yellow";
+  }
+
+}, false);
+
+document.addEventListener("dragleave", function(event) {
+  // reset background of potential drop target when the draggable element leaves it
+  if (event.target.className == "dropzone") {
+    event.target.style.background = "";
+  }
+
+}, false);
+
+document.addEventListener("drop", function(event) {
+  // prevent default action (open as link for some elements)
+  event.preventDefault();
+  // move dragged elem to the selected drop target
+  if (event.target.className == "dropzone") {
+    event.target.style.background = "";
+    dragged.parentNode.removeChild( dragged );
+    event.target.appendChild( dragged );
+  }
+}, false);
